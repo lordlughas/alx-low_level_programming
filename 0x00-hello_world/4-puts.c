eddfa12e60763bbcb7a480b309d21c39167c70c9
Charles Lughas <lordlughas@gmail.com>
2022-09-08 16:49:58 -0700

    Creates C program to print text of a file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - prints a string
  *
- * Return:0;
+ * Return: 0;
  */
-int main(void);
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
