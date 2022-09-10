36b9a4a2ae5a88ac646fa4f68d3ee1e906f3e2c7
Charles Lughas <lordlughas@gmail.com>
2022-09-09 18:09:10 -0700

    prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+/**
+ * main - main entry point
+ *
+ * Description: program that prints the lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
