6bdf5b35ca0c2119a32d127cd84083a65a035d69
Charles Lughas <lordlughas@gmail.com>
2022-09-10 07:21:36 -0700

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,32 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-* main - main entry point
-*
-* Description: prints all possible different combinations of three digits
-* Return Always (Success)
+ * main - main entry block
+ *
+ * Description: prints all possible different combinations of two digits
+ * Return Always 0 (Success)
+ */
 int main(void)
 {
- int c, i, k;
+	int c, i;
 
-for (c = '0'; c <= '9'; c++)
-{
- for (i = '0'; i <= '9'; i++)
- {
-   for (k = '0'; k <= '9'; i++)
-   {
-     if (c < i && i < k)
-      {
-        putchar(c);
-	putchar(i);
-	putchar(k);
-	if (c != '7')
-	  {
-            putchar(',');
-	    putchar(' '); 
-	  }
-      } 
-   }
- }
-	   putchar('\n');
-	   return (0);
+	for (c = '0'; c <= '9'; c++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
 
-}
-
-
-
-
-
-
-
-
-
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
