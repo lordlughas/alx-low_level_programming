5ab0dbba7ada4c54936c9aa163acf7ad631836b8
Charles Lughas <lordlughas@gmail.com>
2022-09-13 14:52:56 -0700

    Added task 2 to my project

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,17 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int i;
+	int i = 0;
 
-	for (i = '0'; i <= '9'; i++)
+	while (i <= '9')
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
+
 		_putchar('\n');
+
+		i++;
 	}
 }
