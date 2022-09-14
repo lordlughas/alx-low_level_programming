44ea75906e3b3a2f547f98e958df85206d76ab6d
Charles Lughas <lordlughas@gmail.com>
2022-09-13 18:23:36 -0700

    Added task 7

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main -  function that checks for lowercase character
+ *
+ * Return: Always int
+ */
+int print_last_digit(int n)
+{
+	int print_last_digit(int);
+	return (int);
+}
