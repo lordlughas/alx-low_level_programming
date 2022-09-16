1170b9486c773593ae7dc3faa6aab02123877320
Charles Lughas <lordlughas@gmail.com>
2022-09-15 17:23:35 -0700

    Printed FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - fizz buzz code
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 0)
+		{
+			printf(' ');
+		}
+	}
+	printf('\n');
+	return (0);
+}
