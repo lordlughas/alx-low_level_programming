965a61f3c7331d749721fe459c7ef81f5cec8506
Charles Lughas <lordlughas@gmail.com>
2022-09-16 05:40:01 -0700

    Updated task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,18 +13,19 @@ void more_numbers(void)
 
 	while (i < 10)
 	{
-		for (n = '0'; n <= '14'; n++)
+		for (n = 0; n <= 14; n++)
 		{
 			c = n;
-			if (n > '9')
+			if (n > 9)
 			{
 				_putchar('1');
 				c = n % 10;
 			}
-		}
+
 			_putchar('0' + c);
+		}
 
 			_putchar('\n');
 			i++;
-
 	}
+}
