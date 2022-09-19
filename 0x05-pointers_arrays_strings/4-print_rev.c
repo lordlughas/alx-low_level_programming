a87b0fe8d12de5e38a1c4da42581270fc990b2a3
Charles Lughas <lordlughas@gmail.com>
2022-09-19 15:23:23 -0700

    Upadated task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,27 +1,18 @@
 #include "main.h"
 
 /**
- * rev_string - prints a string in reverse
- * @s: the used string to beb reversed
- *
+ * print_rev - prints a string in reverse
+ * @s: the used string reference pointer
  * Return: 0
  */
 
-void rev_string(char *s)
+void print_rev(char *s)
 {
-	int len, i, half;
-	char temp;
+	int i = 0;
 
-	for (len = 0; s[len] != '\0'; len++)
-		;
-	i = 0;
-	half = len / 2;
-
-	while (half--)
-	{
-		temp = s[len - i - 1];
-		s[len - i - 1] = s[i];
-		s[i] = temp;
+	while (s[i])
 		i++;
-	}
+	while (i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
