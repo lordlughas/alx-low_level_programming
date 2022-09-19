f287b57f2cfe3814401d6c1480391d871e28095a
Charles Lughas <lordlughas@gmail.com>
2022-09-19 13:49:31 -0700

    Added task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ * 
+ * Return: int length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while(s[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
