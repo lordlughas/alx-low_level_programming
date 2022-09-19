c2e19d25b9155a99a932a0dc3b8d623f07f17ebc
Charles Lughas <lordlughas@gmail.com>
2022-09-19 15:06:32 -0700

    Added task 4 file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,19 +2,19 @@
 
 /**
  * rev_string - prints a string in reverse
- * @s: string to be reversed
+ * @s: the used string to beb reversed
  *
- * Return: nothing
+ * Return: 0
  */
 
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int len, i, half;
 	char temp;
 
 	for (len = 0; s[len] != '\0'; len++)
-
-		i = 0;
+		;
+	i = 0;
 	half = len / 2;
 
 	while (half--)
@@ -24,5 +24,4 @@ void print_rev(char *s)
 		s[i] = temp;
 		i++;
 	}
-
 }
