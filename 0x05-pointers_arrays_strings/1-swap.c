ced78a929644ba9fdb7a8359369af39872fb7c36
Charles Lughas <lordlughas@gmail.com>
2022-09-19 18:29:59 -0700

    Fixed erro on function

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /*
- * swap_int - swaps the values of a and b
- * @a: swap integer 1
- * @b: swap integer 2
+ * swap_int - Swaps the values of two integers
+ * @a: to be swapped with b
+ * @b: to be swapped with a
  *
- * Return: 0
+ * Return: no return
  */
 
 void swap_int(int *a, int *b)
