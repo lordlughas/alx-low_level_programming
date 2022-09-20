4ecd6688ab26c29db7006f5faa062fcfc791d240
Charles Lughas <lordlughas@gmail.com>
2022-09-19 19:28:48 -0700

    Fixed betty error in task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-/*
- * swap_int - Swaps the values of two integers
+/**
+ * swap_int - Swap the value of two integers
  * @a: to be swapped with b
  * @b: to be swapped with a
- *
  * Return: no return
  */
 void swap_int(int *a, int *b)
