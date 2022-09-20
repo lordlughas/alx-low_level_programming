be5d9bada83d017a120fef13d50589fb997c35b7
Charles Lughas <lordlughas@gmail.com>
2022-09-19 19:35:12 -0700

    Fixed betty doc error in task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /*
- * main - updates the value n integer to 98
+ * reset_to_98 - Updates the value n integer to 98
  * @n: integer pointer to a variable that is updated
- * reset_to_98 - function to update n integer
- *
  * Description: function that takes a pointer to an int as parameter
  * and updates the value it points to to 98
  *
  * Return: nothing
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
