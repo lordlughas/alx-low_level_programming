64b57ce64380d219d28fd3c4c565ca60e9b7c5c1
Charles Lughas <lordlughas@gmail.com>
2022-09-22 16:34:22 -0700

    Fixed error in running code

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,11 @@
+#include <ctype.h>
 #include "main.h"
 
 /**
  * string_toupper - change all lowercase to uppercase letters
- * Return: void
+ * of a string to uppercase
+ * @index: The string to be changed
+ * Return: A pointer to the changed string
  */
 
 char *string_toupper(char *)
@@ -15,9 +18,9 @@ char *string_toupper(char *)
 		if (str[index] >= 'a' && str[index] <= 'z')
 			str[index] -= 32;
 
-		inidex++;
+		index++;
 	}
-		
 
-	return(str);
+
+	return (str);
 }
