ec1a5a05e0b55a9a3af383aa87e034322717bb1e
Charles Lughas <lordlughas@gmail.com>
2022-09-23 10:05:44 -0700

    Added advanced task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -23,7 +23,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	if (l + 1 > size_r)
 		return (0);
 	r[l] = '\0';
-
 	for (k = l - 1 ; k >= 0 ; k--)
 	{
 		i--;
@@ -39,7 +38,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[k] = (f + s + d) % 10 + '0';
 		d = (f + s + d) / 10;
 	}
-
 	if (d == 1)
 	{
 		r[l + 1] = '\0';
