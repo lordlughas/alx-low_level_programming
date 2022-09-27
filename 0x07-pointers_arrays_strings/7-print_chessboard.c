efb38a453ad662313f38d91c643b608fc90e8c7a
Charles Lughas <lordlughas@gmail.com>
2022-09-26 17:00:35 -0700

    wrote a function to print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,6 +3,7 @@
 /**
  * print_chessboard - function that prints chessboard
  * @a: array variable
+ * Return: nothing
  */
 void print_chessboard(char (*a)[8])
 {
@@ -12,8 +13,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (y = 0; y < 8; y++)
 		{
-			putchar(a[x][y]);
+			_putchar(a[x][y]);
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
