51d5bc3e56741a16cb09093211895bb540026c4c
Charles Lughas <lordlughas@gmail.com>
2022-09-26 17:58:42 -0700

    Added code to print diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int x, int sum1 = 0, int sum2 = 0;
+	int x, sum1 = 0, sum2 = 0;
 
 	for (x = 0; x < size; x++)
 	{
-	sum1 += a[(size + 1) * b];
-	sum2 += a[(size - 1) * (b + 1)];
+	sum1 += a[(size + 1) * x];
+	sum2 += a[(size - 1) * (x + 1)];
 	}
 	printf("%d,%d\n", sum1, sum2);
 }
