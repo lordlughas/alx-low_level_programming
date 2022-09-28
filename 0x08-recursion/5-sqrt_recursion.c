58c5fa107b9ad8fef49f7bbc89d942fda8344d0b
Charles Lughas <lordlughas@gmail.com>
2022-09-28 16:59:37 -0700

    Printed natura sqrt of number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -27,9 +27,11 @@ int check_sqrt(int n, int rt)
  */
 int _sqrt_recursion(int n)
 {
+	rt = 0;
+
 	if (n < 0)
 		return (-1);
-	if (n == 0 || n == 1)
-		return (n);
-	return (check_sqrt(n, 2));
+	if (n == 1)
+		return (1);
+	return (check_sqrt(n, rt));
 }
