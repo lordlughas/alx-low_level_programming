d73e6799e12ac8dba1dda85f942961c09e901e73
Charles Lughas <lordlughas@gmail.com>
2022-10-10 16:24:39 -0700

    Added task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "2-main.h"
+
+/**
+ * filename: print filename
+ * return - always 0
+ */
+void filename(void)
+{
+	printf("%d\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/2-main.h
@@ -0,0 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#define __FILE__
+
+#endif /* _MAIN_H_ */
