9bfe7b7b944e827087eef388192848268e946a72
Charles Lughas <lordlughas@gmail.com>
2022-10-10 19:12:22 -0700

    Last task

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+#include "dog.h"
+
+
+
+/**
+
+ * free_dog - free dog struct
+
+ * @d: dog to free
+
+ */
+
+void free_dog(dog_t *d)
+
+{
+
+	if (d == NULL)
+
+		return;
+
+	free(d->name);
+
+	free(d->owner);
+
+	free(d);
+
+}
