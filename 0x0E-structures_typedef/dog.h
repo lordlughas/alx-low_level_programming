515d17f3c38c3404e5d5136609382dc6e747cbc5
Charles Lughas <lordlughas@gmail.com>
2022-10-10 18:53:05 -0700

    Added task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - creates a struct with members
+ * @name: one of the members of the struct
+ * @age: one of the members of the struct
+ * @owner: one of the members of the struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
