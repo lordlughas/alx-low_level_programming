bb417126a744d2edf125f2ec5114e01b7658a828
Charles Lughas <lordlughas@gmail.com>
2022-10-13 16:41:33 -0700

    Added task 3 files

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - compute sum of two ints
+ * @num1: int to add to num2
+ * @num2: int to add to num1
+ * Return: sum of a and b
+ */
+int op_add(int num1, int num2)
+{
+	return (num1 + num2);
+}
+
+/**
+ * op_sub - compute difference of two ints
+ * @num1: int to subtract with num2
+ * @num2: int to subtract from num1
+ * Return: difference of num1 and num2
+ */
+int op_sub(int num1, int num2)
+{
+	return (num1 - num2);
+}
+
+/**
+ * op_mul - compute product of two ints
+ * @num1: int to multiply with num2
+ * @num2: int to multiply with num1
+ * Return: product of num1 and num2
+ */
+int op_mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
+
+/**
+ * op_div - compute quotient of two ints
+ * @num1: int to divide by num2
+ * @num2: int to divide num1 by num2
+ * Return: quotient of num1 and num2
+ */
+int op_div(int num1, int num2)
+{
+	return (num1 / num2);
+}
+
+/**
+ * op_mod - compute remainder of two ints
+ * @num1: int to mod with num2
+ * @num2: int to mod num1 num2
+ * Return: remainder of num1 and num2
+ */
+int op_mod(int num1, int num2)
+{
+	return (num1 % num2);
+}
