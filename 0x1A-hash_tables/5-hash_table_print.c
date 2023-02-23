7d9bd4702c5ac23d8ab3a2b83824c183a0daa70c
Charles Lughas <lordlughas@gmail.com>
2023-02-22 21:34:15 -0800

    hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,31 +1,39 @@
-#include "hash_tables.h" 
-  
- /** 
-  * hash_table_print - prints a hash table 
-  * @ht: hash table to print 
-  * 
-  * Return: void 
-  */ 
- void hash_table_print(const hash_table_t *ht) 
- { 
-         unsigned long int i; 
-         hash_node_t *tmp; 
-         char flag = 0; /* 0 while no data has been printed yet */ 
-  
-         if (ht == NULL || ht->array == NULL) 
-                 return; 
-         printf("{"); 
-         for (i = 0; i < ht->size; i++) 
-         { 
-                 tmp = ht->array[i]; 
-                 while (tmp != NULL) 
-                 { 
-                         if (flag == 1) 
-                                 printf(", "); 
-                         printf("'%s': '%s'", tmp->key, tmp->value); 
-                         flag = 1; 
-                         tmp = tmp->next; 
-                 } 
-         } 
-         printf("}\n"); 
- }
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ *
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char comma_flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
+		}
+	}
+	printf("}\n");
+}
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,31 +1,30 @@
-#include "hash_tables.h" 
-  
- /** 
-  * hash_table_delete - deletes a hash table 
-  * @ht: hash table to delete 
-  * 
-  * Return: void 
-  */ 
- void hash_table_delete(hash_table_t *ht) 
- { 
-         unsigned long int i; 
-         hash_node_t *next; 
-  
-         if (ht == NULL || ht->array == NULL || ht->size == 0) 
-                 return; 
-         for (i = 0; i < ht->size; i++) 
-         { 
-                 while (ht->array[i] != NULL) 
-                 { 
-                         next = ht->array[i]->next; 
-                         free(ht->array[i]->key); 
-                         free(ht->array[i]->value); 
-                         free(ht->array[i]); 
-                         ht->array[i] = next; 
-                 } 
-         } 
-         free(ht->array); 
-         ht->array = NULL; 
-         ht->size = 0; 
-         free(ht); 
- }
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
