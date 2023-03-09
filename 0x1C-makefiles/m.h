5ea42b59b6ba311f85c29a8f27ef309c3f1d0821
Charles Lughas <lordlughas@gmail.com>
2023-03-09 08:42:04 -0800

    Hot fix

--- 0x1C-makefiles/m.h
+++ 0x1C-makefiles/m.h
@@ -1,9 +1,9 @@
-#ifndef __M_H__ 
-#define __M_H__ 
-  
-#include <stdlib.h> 
-#include <stdio.h> 
-  
-void print_school(void); 
-  
+#ifndef __M_H__
+#define __M_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_school(void);
+
 #endif
